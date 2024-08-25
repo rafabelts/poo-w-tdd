cca114f7c46b3b5289c20b3be3b7b06f655ab899
Rafael Beltran <rafabeltrans17@gmail.com>
2024-08-24 21:19:27 -0600

    methods commented

--- hola-mundo/app/src/main/java/org/example/App.java
+++ hola-mundo/app/src/main/java/org/example/App.java
@@ -9,15 +9,18 @@ public class App {
     private String SPANISH = "spanish";
     private String FRENCH = "french";
 
+    // Creamos el hash map (estructura de datos) de los prefijos
     Map<String, String> prefixes = new HashMap<>();
 
     public App() {
+        // Agregamos los datos al mapa .put(llave, valor)
         prefixes.put(ENGLISH, "Hello, ");
         prefixes.put(SPANISH, "Hola, ");
         prefixes.put(FRENCH, "Bonjour, ");
     }
 
     public String getPrefix(String language) {
+        // Rule switch para retornar el prefijo de acuerdo al lenguaje
         switch (language) {
             case "spanish" -> {
                 return prefixes.get(SPANISH);
@@ -32,11 +35,23 @@ public class App {
     }
 
     public String getGreeting(String name, String language) {
+        // Obtenemos el prefijo
         String prefix = getPrefix(language);
+
+        /*
+            Condicional para verificar que si el nombre este vacio entonces retorne
+            "Hello, World!"
+
+            En Java existe un metodo para verificar que string 1 sea igual que un string 2
+            string1.equals(string2) -> boolean (true or false)
+            si son iguales retorna true
+            sino retorna false
+        */
         if (name.equals("")) {
             return "Hello, World!";
         }
 
+        // Retornamos el prefijo y el nombre
         return prefix + name;
     }
 }
--- hola-mundo/app/src/test/java/org/example/AppTest.java
+++ hola-mundo/app/src/test/java/org/example/AppTest.java
@@ -7,13 +7,19 @@ import org.junit.jupiter.api.Test;
 
 class AppTest {
 
+    /* Creo instancia de la clase App la cual se probara, esta funciona como
+     campo de la clase AppTest */
     App classToTest = new App();
 
+    // Anotacion de que la función es un test
     @Test
+    // Nombre a mostrar tests
     @DisplayName("Test get prefix")
     void testGetPrefix() {
+        // Probamos el metodo del prefijo en idioma frances
         String got = classToTest.getPrefix("french");
 
+        // Checamos que el resultado sea lo que esperabamos
         assertEquals(
             "Bonjour, ",
             got,
@@ -25,8 +31,11 @@ class AppTest {
     @DisplayName("Test hola con name vacio")
     void testGreetingVoid() {
         String name = "";
+
+        // Probamos el metodo del saludo con nombre e idioma vacio
         String got = classToTest.getGreeting(name, "");
 
+        // Checamos que el resultado sea lo que esperabamos
         assertEquals(
             "Hello, World!",
             got,
@@ -38,8 +47,10 @@ class AppTest {
     @DisplayName("Test hola")
     void testGreeting() {
         String name = "Rafa";
+        // Probamos el metodo del saludo con un nombre
         String got = classToTest.getGreeting(name, "spanish");
 
+        // Checamos que el resultado sea lo que esperabamos
         assertEquals(
             "Hola, " + name,
             got,
